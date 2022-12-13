421e8d0f2d58863a479057e36c3f7633750c6eed
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-12-13 13:39:10 +0200

    Add files via upload

---
+++ Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/06SumOfNumbersNM.js
@@ -0,0 +1,10 @@
+function sumOfNumbersNM(n, m) {
+    n = Number(n);
+    m = Number(m);
+    let result = 0;
+    for (let i = n; i <= m; i++) {
+        result += i;
+    }
+    console.log(result);
+}
+sumOfNumbersNM('-8', '20');
