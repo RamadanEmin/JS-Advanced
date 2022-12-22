1db40f16bdfe0bf5669313c7b1bbf2e141a0f231
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-12-22 16:20:14 +0200

    Create EditElement.js

---
+++ DOM Introduction/DOM Introduction - Lab/01. Edit Element/EditElement.js
@@ -0,0 +1,7 @@
+function editElement(element, match, replacer) {
+    // while (element.textContent.includes(match)) {
+    //     element.textContent = element.textContent.replace(match, replacer);
+    // }
+    let pattern = new RegExp(match,'g');
+    element.textContent = element.textContent.replace(pattern, replacer);
+}
