20d03526b0009fd63c32ba9b4757ca99d022aae6
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2022-12-30 09:25:47 +0200

    Add files via upload

---
+++ Advanced Functions/Advanced Functions - Lab/05CommandProcessor.js
@@ -0,0 +1,40 @@
+function solution() {
+    let str = '';
+    return {
+        append,
+        removeStart,
+        removeEnd,
+        print
+    };
+    function append(strToAppend) {
+        str += strToAppend;
+    }
+    function removeStart(n) {
+        str = str.substring(n);
+    }
+    function removeEnd(n) {
+        str = str.substring(0, str.length - n);
+    }
+    function print() {
+        console.log(str);
+    }
+    // return {
+    //     append: (s) => str += s,
+    //     removeStart: (n) => str = str.slice(n),
+    //     removeEnd: (n) => str = str.slice(0, -n),
+    //     print: () => console.log(str)
+    // };
+}
+let firstZeroTest = solution();
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
+
+let secondZeroTest = solution();
+secondZeroTest.append('123');
+secondZeroTest.append('45');
+secondZeroTest.removeStart(2);
+secondZeroTest.removeEnd(1);
+secondZeroTest.print();
