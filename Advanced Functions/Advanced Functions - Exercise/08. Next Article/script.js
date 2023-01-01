faf498e82fc1b2f5b36775a8a7ac79472bdcd4d0
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2023-01-01 10:20:59 +0200

    Add files via upload

---
+++ Advanced Functions/Advanced Functions - Exercise/08. Next Article/script.js
@@ -0,0 +1,11 @@
+function getArticleGenerator(articles) {
+    const div = document.getElementById('content');
+    return function () {
+        const firstElement = articles.shift();
+        if (firstElement) {
+            const article = document.createElement('article');
+            article.textContent = firstElement;
+            div.appendChild(article);
+        }
+    }
+}
